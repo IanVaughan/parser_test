f70eff8ab00313046facacba28b880982394c5b5
Ian Vaughan <github@ianvaughan.co.uk>
2023-01-14 18:34:13 +0000

    Add calls to parser bin runner

--- parser.rb
+++ parser.rb
@@ -1,9 +1,19 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+# Usage: ./parser.rb webserver.log
+
+require './lib/parser'
+require './lib/presenter'
+
 file_input = ARGV[0]
 
-File.foreach(file_input) do |line|
-  puts line
-end
+parser = Parser.new
+File.foreach(file_input) { |line| parser.parse(line) }
 
+puts 'Visits:'
+puts Presenter.new(parser.visits).visits
+
+puts
+puts 'Unique:'
+puts Presenter.new(parser.unique).unique
